15a1c35c8c369df6d72932fb38c2489972b13f75
meblo98 <lomouhamedelbachir@gmail.com>
2024-08-06 12:38:41 +0000

    affichage des etudiants

---
+++ app/Http/Controllers/EtudiantController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreEtudiantRequest;
+use App\Http\Requests\UpdateEtudiantRequest;
+use App\Models\Etudiant;
+
+class EtudiantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $etudiant = Etudiant::all();
+        return $this->customJsonResponse("Liste des Étudiants", $etudiant);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEtudiantRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEtudiantRequest $request, Etudiant $etudiant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Etudiant $etudiant)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,23 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EtudiantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('etudiants', [EtudiantController::class, 'index']);
+
+
 Route::post("login", [AuthController::class, "login"]);
 Route::middleware("auth")->group(
     function () {
         Route::get("logout", [AuthController::class, "logout"]);
-
+        Route::get("refresh", [AuthController::class, "refreshToken"]);
     }
 );
